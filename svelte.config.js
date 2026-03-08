24d04c66a9c4d332e317cd6c51bab6fb6e779ccb
Grant Ralls <grantsralls@gmail.com>
2026-03-08 18:04:18 -0400

    changing basepath

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter({
-      fallback: '404.html'
+      fallback: '404.html',
+      pages: 'build'
     }),
     paths: {
       base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
